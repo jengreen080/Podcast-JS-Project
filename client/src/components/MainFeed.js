7819e1d3f310decb78d27a71faa82b5d6332d01f
Ben Bilgili <benbilgili@gmail.com>
2023-06-07 09:28:13 +0100

    increment

--- client/src/components/MainFeed.js
+++ client/src/components/MainFeed.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import TileComponent from './TileComponent';
 import styled from 'styled-components'
+import LikeButton from './LikeButton';
 
 const StyledTileComponent = styled.div`
 background-color: thistle;
@@ -15,30 +16,38 @@ box-shadow: rgba(60, 64, 67, 0.3) 0px 1px 2px 0px, rgba(60, 64, 67, 0.15) 0px 2p
 const MainFeed = ({likeCounter, setLikeCounter, likeButtonText, setLikeButtonText, allReviews}) => {
 
   const reviewElements = allReviews.map((review) => {
+
+    console.log(likeButtonText)
+
     return (
-      <li className='review-tile' >
-        <h3>{review.username}</h3>
-        <h5>{review.podcast.name}</h5>
-        <p>{review.text}</p>
-        {/* <img src={review.podcast.imgUrl} /> */}
-      </li>
+      <StyledTileComponent>
+        <li className='review-tile' >
+          <h3>{review.username}</h3>
+          <h5>{review.podcast.name}</h5>
+          <p>{review.text}</p>
+          {/* <img src={review.podcast.imgUrl} /> */}
+          <LikeButton>{likeButtonText}</LikeButton>
+          <button>Comment</button>
+        </li>
+        
+      </StyledTileComponent>
     )
   })
 
   return (
-    <div>
+    
       <ul>
         {reviewElements}
       </ul>
-    </div>
-    // <StyledTileComponent>
+      
+    // 
     //   <TileComponent 
     //   likeCounter={likeCounter} 
     //   setLikeCounter= {setLikeCounter}
     //   likeButtonText ={likeButtonText}
     //   setLikeButtonText = {setLikeButtonText}
     //   />
-    // </StyledTileComponent>
+    // 
   );
 }
 
